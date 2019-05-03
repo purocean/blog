da04d86c27ca4d0c1140266276710cef1a12cd5a
purocean <purocean@gmail.com>
2019-05-03 18:56:02 +0800

    增加统计代码

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,6 +2,9 @@ module.exports = {
   host: 'localhost',
   port: 8000,
   dest: './docs',
+  head: [
+    ['script', { src: 'http://tajs.qq.com/stats?sId=66403914', charset: 'UTF-8' }]
+  ],
   title: '洋子的博客',
   description: '^_^',
   themeConfig: {
