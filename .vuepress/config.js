ea6a10ca6d223f1540d92cea21d6fd4075bff378
purocean <purocean@gmail.com>
2019-05-03 20:21:49 +0800

    deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,8 +5,8 @@ module.exports = {
   head: [
     ['script', { src: 'http://tajs.qq.com/stats?sId=66403914', charset: 'UTF-8' }]
   ],
-  title: '洋子的博客',
-  description: '^_^',
+  title: '洋子的自留地',
+  description: '互联网的搬运工',
   themeConfig: {
     repo: 'purocean/blog',
     lastUpdated: 'Last Updated',
