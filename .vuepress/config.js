e234711a3b54ee01f309ee234143362eb9eb3026
purocean <purocean@gmail.com>
2019-05-03 18:40:18 +0800

    增加文件

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  host: 'localhost',
+  port: 8000,
+  dest: './docs',
+  title: '洋子的博客',
+  description: '^_^',
+  themeConfig: {
+    repo: 'purocean/blog',
+    lastUpdated: 'Last Updated',
+    sidebar: 'auto',
+  }
+}
